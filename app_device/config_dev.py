e75dc65d62a0b3d7645d676e1e45add4ce995efc
sbonnaud1 <serge.bonnaud@fr.ibm.com>
2020-04-18 16:00:16 +0200

    Update config_dev.py

--- app_device/config_dev.py
+++ app_device/config_dev.py
@@ -1,9 +1,9 @@
 device_configuration = {
-	"org_id"           : "abcd1z",             # replace with your organization ID
+	"org_id"           : "uz6g30",             # replace with your organization ID
 	"domain"           : "internetofthings.ibmcloud.com",
 	"device_type"      : "MotionDevice",       # replace with your device type ID
-	"device_id"        : "my-phone",           # replace with your device ID
-	"dev_auth_token"   : "$9HjMsPk8w1jVb7knd", # replace with your device token
+	"device_id"        : "terminalPython",           # replace with your device ID
+	"dev_auth_token"   : "!9!J8_rx0PBDZr9v)h", # replace with your device token
 	"qos"              : 2,
 	"disconnect_after" : 20,  # (in seconds)
 	"throttleInterval" : 1000 # (in milliseconds)
