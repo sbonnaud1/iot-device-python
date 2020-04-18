06d1f3059f98e8667b4b3135cd92ebd8ff01801d
sbonnaud1 <serge.bonnaud@fr.ibm.com>
2020-04-18 17:44:50 +0200

    Update config_dev.py

--- app_device/config_dev.py
+++ app_device/config_dev.py
@@ -2,8 +2,8 @@ device_configuration = {
 	"org_id"           : "uz6g30",             # replace with your organization ID
 	"domain"           : "internetofthings.ibmcloud.com",
 	"device_type"      : "LabPythonDevice",       # replace with your device type ID
-	"device_id"        : "terminalPython",           # replace with your device ID
-	"dev_auth_token"   : "t1nM4bfrmRcBc1BUHK", # replace with your device token
+	"device_id"        : "IoTDevice1",           # replace with your device ID
+	"dev_auth_token"   : "430FEYckMPx5Ovw7mr", # replace with your device token
 	"qos"              : 2,
 	"disconnect_after" : 20,  # (in seconds)
 	"throttleInterval" : 1000 # (in milliseconds)
